06b8bce7ac95e731b16562ec2c43a18a225c1848
yapjinai <jinai.jyap@gmail.com>
2019-02-05 22:23:50 -0500

    add delete and create to todos controller

--- app/controllers/api/v1/todos_controller.rb
+++ app/controllers/api/v1/todos_controller.rb
@@ -3,4 +3,26 @@ class Api::V1::TodosController < ApplicationController
   def index
     render json: Todo.all
   end
+
+  def create
+    @todo = Todo.create(todo_params)
+    byebug
+    if @todo.valid?
+      render json: { todo: TodoSerializer.new(@todo) }, status: :created
+    else
+      render json: { error: 'failed to create todo' }, status: :not_acceptable
+    end
+  end
+
+  def destroy
+    @note = Note.find(params[:id])
+    @note.destroy
+    render 'deleted note'
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:id, :user_id, :item_id)
+  end
 end
